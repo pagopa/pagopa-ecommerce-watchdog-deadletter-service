6ba404aea655963043e84f02deda1ad3b8449d77
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-07 16:28:36 +0200

    feat(jwt): add Azure KeyVault config

---
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/AzureConfig.kt
@@ -0,0 +1,55 @@
+package it.pagopa.ecommerce.watchdog.deadletter.config.jwt
+
+import com.azure.core.http.policy.FixedDelay
+import com.azure.core.http.policy.RetryPolicy
+import com.azure.identity.DefaultAzureCredentialBuilder
+import com.azure.security.keyvault.certificates.CertificateAsyncClient
+import com.azure.security.keyvault.certificates.CertificateClientBuilder
+import com.azure.security.keyvault.secrets.SecretAsyncClient
+import com.azure.security.keyvault.secrets.SecretClientBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Duration
+
+@Configuration
+class AzureConfig(
+    @Autowired private val azureKeyVaultClientConfigProperties: AzureKeyVaultClientConfigProperties
+) {
+
+    @Bean
+    fun azureKeyVaultSecretClient(): SecretAsyncClient {
+        return SecretClientBuilder()
+            .vaultUrl(azureKeyVaultClientConfigProperties.endpoint)
+            .retryPolicy(
+                RetryPolicy(
+                    FixedDelay(
+                        azureKeyVaultClientConfigProperties.maxRetries, // Maximum retries
+                        Duration.ofMillis(
+                            azureKeyVaultClientConfigProperties.retryDelayMillis
+                        ), // Delay between retries in milliseconds
+                    )
+                )
+            )
+            .credential(DefaultAzureCredentialBuilder().build())
+            .buildAsyncClient()
+    }
+
+    @Bean
+    fun azureKeyVaultCertClient(): CertificateAsyncClient {
+        return CertificateClientBuilder()
+            .vaultUrl(azureKeyVaultClientConfigProperties.endpoint)
+            .retryPolicy(
+                RetryPolicy(
+                    FixedDelay(
+                        azureKeyVaultClientConfigProperties.maxRetries, // Maximum retries
+                        Duration.ofMillis(
+                            azureKeyVaultClientConfigProperties.retryDelayMillis
+                        ), // Delay between retries in milliseconds
+                    )
+                )
+            )
+            .credential(DefaultAzureCredentialBuilder().build())
+            .buildAsyncClient()
+    }
+}
