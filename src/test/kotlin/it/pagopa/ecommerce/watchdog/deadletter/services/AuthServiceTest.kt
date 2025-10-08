adae5a1bcd81e462bb92b42a8144d6066b7f662a
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-08 10:52:25 +0200

    test(auth): refactor auth tests

--- src/test/kotlin/it/pagopa/ecommerce/watchdog/deadletter/services/AuthServiceTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/watchdog/deadletter/services/AuthServiceTest.kt
@@ -1,24 +1,22 @@
 package it.pagopa.ecommerce.watchdog.deadletter.services
 
+import it.pagopa.ecommerce.watchdog.deadletter.domain.UserDetails
 import it.pagopa.generated.ecommerce.watchdog.deadletter.v1.model.AuthenticationCredentialsDto
 import org.junit.jupiter.api.Test
 import reactor.test.StepVerifier
 
 class AuthServiceTest {
-    private val loginRedirectUrl = "http://localhost/"
-
-    private val authService: AuthService = AuthService(loginRedirectUrl)
+    private val authService: AuthService = AuthService()
 
     @Test
     fun `should authenticate user`() {
         // pre-requisites
         val credentials = AuthenticationCredentialsDto("user", "password")
+        val userDetails = UserDetails("12345", "Mario", "Rossi", "mock@email.com")
 
         // test
         StepVerifier.create(authService.authenticateUser(credentials))
-            .expectNextMatches { authenticationOkDto ->
-                authenticationOkDto.urlRedirect == "$loginRedirectUrl#token=123"
-            }
+            .expectNext(userDetails)
             .verifyComplete()
     }
 }
