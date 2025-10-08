cd031dceacaf016b39ef8abae1e5f7be854c96e9
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-08 10:53:01 +0200

    test(exceptions): add exception handler test

---
+++ src/test/kotlin/it/pagopa/ecommerce/watchdog/deadletter/exception/ExceptionHandlerTest.kt
@@ -0,0 +1,48 @@
+package it.pagopa.ecommerce.watchdog.deadletter.exception
+
+import it.pagopa.ecommerce.watchdog.deadletter.WatchdogDeadletterTestUtils
+import it.pagopa.ecommerce.watchdog.deadletter.exception.handler.ExceptionHandler
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+
+class ExceptionHandlerTest {
+
+    private val exceptionHandler = ExceptionHandler()
+
+    @Test
+    fun `Should handle RestApiException`() {
+        val response =
+            exceptionHandler.handleException(
+                RestApiException(
+                    httpStatus = HttpStatus.BAD_REQUEST,
+                    title = "title",
+                    description = "description"
+                )
+            )
+        assertEquals(
+            WatchdogDeadletterTestUtils.buildProblemJson(
+                httpStatus = HttpStatus.BAD_REQUEST,
+                title = "title",
+                description = "description"
+            ),
+            response.body
+        )
+        assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
+    }
+
+    @Test
+    fun `Should handle ApiError`() {
+        val exception = UserUnauthorizedException()
+        val response = exceptionHandler.handleException(exception)
+        assertEquals(
+            WatchdogDeadletterTestUtils.buildProblemJson(
+                httpStatus = HttpStatus.UNAUTHORIZED,
+                title = "User unauthorized",
+                description = "User is not authorized to access the resource"
+            ),
+            response.body
+        )
+        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+}
