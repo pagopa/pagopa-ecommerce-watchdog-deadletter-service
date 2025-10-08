ee2133062e32e212c6c7ee713d1b11b3b182ccd6
sterondinella <stefano.rondinella@pagopa.it>
2025-10-08 09:52:34 +0200

    fix(spotless): apply spotlessKotlin

--- src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/services/AuthService.kt
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/services/AuthService.kt
@@ -10,6 +10,10 @@ import reactor.core.publisher.Mono
 class AuthService(@Value("\${login.redirectUrl}") private val loginRedirectUrl: String) {
 
     fun authenticateUser(credentials: AuthenticationCredentialsDto): Mono<AuthenticationOkDto> {
-        return Mono.just(AuthenticationOkDto("$loginRedirectUrl#token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlkIiwibmFtZSI6Ik1hcmlvIiwic3VybmFtZSI6IlJvc3NpIiwiZW1haWwiOiJtYXJpby5yb3NzaUBtb2NrLmNvbSIsImlhdCI6MTc1OTc1Mjg3NCwiZXhwIjoxNzU5OTMxOTMzLCJhdWQiOiJ3YXRjaGRvZyIsImlzcyI6IndhdGNoZG9nLWRlYWRsZXR0ZXItc2VydmljZSJ9.PXQlFZYtWo5eb_XuhEvJC3x4vmjtVYKHb01FZ2QIVFw"))
+        return Mono.just(
+            AuthenticationOkDto(
+                "$loginRedirectUrl#token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlkIiwibmFtZSI6Ik1hcmlvIiwic3VybmFtZSI6IlJvc3NpIiwiZW1haWwiOiJtYXJpby5yb3NzaUBtb2NrLmNvbSIsImlhdCI6MTc1OTc1Mjg3NCwiZXhwIjoxNzU5OTMxOTMzLCJhdWQiOiJ3YXRjaGRvZyIsImlzcyI6IndhdGNoZG9nLWRlYWRsZXR0ZXItc2VydmljZSJ9.PXQlFZYtWo5eb_XuhEvJC3x4vmjtVYKHb01FZ2QIVFw"
+            )
+        )
     }
 }
