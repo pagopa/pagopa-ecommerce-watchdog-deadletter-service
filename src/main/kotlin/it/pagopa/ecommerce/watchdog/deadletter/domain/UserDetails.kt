920f5f86405b68cee4c32b92676bea3b654e9421
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-16 14:39:50 +0200

    feat(auth): update UserDetails class to implement Spring security UserDetails interface

--- src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/domain/UserDetails.kt
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/domain/UserDetails.kt
@@ -1,7 +1,16 @@
 package it.pagopa.ecommerce.watchdog.deadletter.domain
 
-data class UserDetails(val id: String, val name: String, val surname: String, val email: String) {
-    fun toMap(): Map<String, String> {
-        return mapOf("id" to id, "name" to name, "surname" to surname, "email" to email)
-    }
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+data class UserDetails(val id: String, val name: String, val surname: String, val email: String) :
+    UserDetails {
+
+    fun toMap(): Map<String, String> = mapOf("id" to id, "name" to name, "surname" to surname, "email" to email)
+
+    override fun getAuthorities(): Collection<GrantedAuthority?> = emptyList()
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String? = null
 }
