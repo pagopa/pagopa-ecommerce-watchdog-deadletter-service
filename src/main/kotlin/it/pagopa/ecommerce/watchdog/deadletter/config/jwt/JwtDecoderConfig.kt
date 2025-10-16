628f53c444b4dc75c831a01974bdbeee69eb2c4a
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-16 15:49:45 +0200

    feat(auth): add method to get authenticated user's id + spotless

--- src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/JwtDecoderConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/JwtDecoderConfig.kt
@@ -1,13 +1,13 @@
 package it.pagopa.ecommerce.watchdog.deadletter.config.jwt
 
 import it.pagopa.ecommerce.watchdog.deadletter.services.jwt.ReactiveAzureKVSecurityKeysService
+import java.security.interfaces.RSAPublicKey
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
-import java.security.interfaces.RSAPublicKey
 
 @Configuration
 class JwtDecoderConfig(
--- src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/services/AuthService.kt
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/services/AuthService.kt
@@ -4,6 +4,7 @@ import it.pagopa.ecommerce.watchdog.deadletter.domain.UserDetails
 import it.pagopa.ecommerce.watchdog.deadletter.exception.UserUnauthorizedException
 import it.pagopa.generated.ecommerce.watchdog.deadletter.v1.model.AuthenticationCredentialsDto
 import org.slf4j.LoggerFactory
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
@@ -18,5 +19,15 @@ class AuthService() {
         else Mono.just(UserDetails("12345", "Mario", "Rossi", "mock@email.com"))
         // TODO: Get user by username from DB
         // TODO: Validate user password
+    }
+
+    fun getAuthenticatedUserId(): Mono<String> {
+        return ReactiveSecurityContextHolder.getContext().flatMap { securityContext ->
+            when (val principal = securityContext.authentication.principal) {
+                is UserDetails -> Mono.just(principal.id)
+                else ->
+                    Mono.error(IllegalStateException("User details not found in security context"))
+            }
+        }
     }
 }
