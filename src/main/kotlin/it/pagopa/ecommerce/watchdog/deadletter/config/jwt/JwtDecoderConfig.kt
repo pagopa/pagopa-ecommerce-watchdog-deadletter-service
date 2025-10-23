ca0816535dcb242c0e4b544d107efc3a623a0d2f
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-23 17:34:39 +0200

    fix(auth): update JwtDecoderConfig to use an ECPublicKey instead of a RSAPublicKey

--- src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/JwtDecoderConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/JwtDecoderConfig.kt
@@ -1,12 +1,17 @@
 package it.pagopa.ecommerce.watchdog.deadletter.config.jwt
 
+import com.nimbusds.jose.jwk.Curve
+import com.nimbusds.jose.jwk.ECKey
+import com.nimbusds.jose.jwk.JWK
+import com.nimbusds.jwt.SignedJWT
 import it.pagopa.ecommerce.watchdog.deadletter.services.jwt.ReactiveAzureKVSecurityKeysService
-import java.security.interfaces.RSAPublicKey
+import java.security.interfaces.ECPublicKey
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import reactor.core.publisher.Flux
 
 @Configuration
 class JwtDecoderConfig(
@@ -15,8 +20,18 @@ class JwtDecoderConfig(
 
     @Bean
     fun jwtDecoder(): ReactiveJwtDecoder {
-        val publicKey = azureKVSecurityKeysService.getPublic().next().block()
-        return NimbusReactiveJwtDecoder.withPublicKey(publicKey!!.publicKey as RSAPublicKey?)
+        val publicKey =
+            azureKVSecurityKeysService.getPublic().next().block()
+                ?: throw IllegalStateException(
+                    "Failed to retrieve public key from Azure KV, result was null."
+                )
+
+        val jwk: JWK =
+            ECKey.Builder(Curve.P_256, publicKey.publicKey as ECPublicKey?)
+                .keyID(publicKey.kid)
+                .build()
+
+        return NimbusReactiveJwtDecoder.withJwkSource { _: SignedJWT? -> Flux.just<JWK?>(jwk) }
             .build()
     }
 }
