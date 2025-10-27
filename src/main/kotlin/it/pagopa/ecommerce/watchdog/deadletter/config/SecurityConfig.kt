8d15dc25688c096dfa92a28bb6e00f3eff99f617
ivan.ferrando <ivan.ferrando@pagopa.it>
2025-10-27 12:35:24 +0100

    feat(security): add first version of security config

---
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/SecurityConfig.kt
@@ -0,0 +1,34 @@
+package it.pagopa.ecommerce.watchdog.deadletter.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig() {
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() } // CSRF is not needed with JWT authentication
+            .cors { it.configurationSource(corsConfigurationSource()) }
+            .authorizeExchange { exchanges -> exchanges.anyExchange().permitAll() }
+            .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedMethods = listOf("GET", "POST", "OPTIONS")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
