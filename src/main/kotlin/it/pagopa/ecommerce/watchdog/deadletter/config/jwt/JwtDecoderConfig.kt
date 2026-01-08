bee2f6913a1ddd99064323fd4fd6c8fcd3960e95
Stefano Rondinella <stefrondinella@live.it>
2026-01-08 19:14:43 +0100

    fix(JwtDecoderConfig): create a class to manage the cached JWT during the validation of the token, add a refreshing process when the validation fail and after a configurable ttl

--- src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/JwtDecoderConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/watchdog/deadletter/config/jwt/JwtDecoderConfig.kt
@@ -3,11 +3,14 @@ package it.pagopa.ecommerce.watchdog.deadletter.config.jwt
 import com.nimbusds.jose.jwk.JWK
 import com.nimbusds.jwt.SignedJWT
 import it.pagopa.ecommerce.watchdog.deadletter.services.ReactiveAzureKVSecurityKeysService
+import java.time.Duration
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm
+import org.springframework.security.oauth2.jwt.BadJwtException
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import reactor.core.publisher.Flux
@@ -15,16 +18,28 @@ import reactor.core.publisher.Mono
 
 @Configuration
 class JwtDecoderConfig(
-    @Autowired private val azureKVSecurityKeysService: ReactiveAzureKVSecurityKeysService
+    @Autowired private val azureKVSecurityKeysService: ReactiveAzureKVSecurityKeysService,
+    @Value("\${auth.jwt.secret.cache.ttl}") private val cacheSecretTtl: Long,
 ) {
 
-    private val logger = LoggerFactory.getLogger(this.javaClass)
+    protected val logger = LoggerFactory.getLogger(this.javaClass)
 
-    @Bean
-    fun jwtDecoder(): ReactiveJwtDecoder {
+    class RefreshableCachedJwkSource(
+        private val azureKVSecurityKeysService: ReactiveAzureKVSecurityKeysService,
+        val cacheTtl: Long,
+    ) {
+        protected val logger = LoggerFactory.getLogger(this.javaClass)
 
-        val cachedKey: Mono<JWK> =
-            azureKVSecurityKeysService
+        @Volatile private var currentCachedMono: Mono<JWK>
+
+        init {
+            // Initialize the value of the current cache value
+            currentCachedMono = createCache()
+        }
+
+        // Generate a new JWT
+        private fun createCache(): Mono<JWK> {
+            return azureKVSecurityKeysService
                 .getPublicJwkFromKeyStore()
                 .doOnSuccess { jwk ->
                     if (jwk == null) {
@@ -35,12 +50,47 @@ class JwtDecoderConfig(
                         logger.debug("JWK DECODER initialized. KID loaded: [${jwk.keyID}]")
                     }
                 }
-                .cache()
+                .cache(
+                    { _ -> Duration.ofMinutes(cacheTtl) },
+                    { _ -> Duration.ZERO }, // No cache if there is an error, instant retry
+                    { Duration.ZERO }, // No cache if is empty
+                )
+        }
 
-        val jwkSource = { _: SignedJWT? -> Flux.from(cachedKey) }
+        fun get(): Mono<JWK> {
+            return currentCachedMono
+        }
 
-        return NimbusReactiveJwtDecoder.withJwkSource(jwkSource)
-            .jwsAlgorithm(SignatureAlgorithm.ES256)
-            .build()
+        fun refresh() {
+            currentCachedMono = createCache()
+        }
+    }
+
+    @Bean
+    fun jwtDecoder(): ReactiveJwtDecoder {
+
+        val cachedJwtResource =
+            RefreshableCachedJwkSource(azureKVSecurityKeysService, cacheSecretTtl)
+
+        val jwkSource = { _: SignedJWT? -> Flux.from(cachedJwtResource.get()) }
+
+        val nimbusDelegate =
+            NimbusReactiveJwtDecoder.withJwkSource(jwkSource)
+                .jwsAlgorithm(SignatureAlgorithm.ES256)
+                .build()
+
+        return ReactiveJwtDecoder { token ->
+            nimbusDelegate.decode(token).onErrorResume(BadJwtException::class.java) { ex ->
+                logger.warn("Error during the validation of the token, possible expired key")
+
+                // Refresh the cached key
+                cachedJwtResource.refresh()
+
+                nimbusDelegate.decode(token).onErrorMap(BadJwtException::class.java) { retryEx ->
+                    logger.error("Validation failed after the refresh of the key!")
+                    retryEx
+                }
+            }
+        }
     }
 }
